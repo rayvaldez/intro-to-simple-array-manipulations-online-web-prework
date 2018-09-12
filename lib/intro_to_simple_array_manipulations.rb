e126f85f9b3f6d233d97065d97c9a7aca181c6c1
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-12 18:01:11 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,7 @@ end
 
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-  chars_arya_killed = chars_in_game_of_thrones.pop(2)
+  chars_arya_killed = chars_in_game_of_thrones.pop 2
 end
 
 def using_shift(im_so_over_this_city)
